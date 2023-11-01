94d095912fd7e1a9b60ac5b5e3e640b0ebca7b3d
Fakhri Hassan Maulana <87844859+dzzlr@users.noreply.github.com>
2023-11-01 17:03:22 +0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,177 +5,233 @@ terraform {
       version = "~> 5.0"
     }
   }
-
-  # backend "s3" {
-  #   bucket = "mycomponents-tfstate"
-  #   key = "state/terraform.tfstate"
-  #   region = "ap-southeast-1"
-  #   encrypt = true
-  #   dynamodb_table = "mycomponents_tf_lockid"
-  # }
 }
 
 provider "aws" {
   region = "ap-southeast-1"
 }
 
-resource "aws_vpc" "landing_zone_vpc" {
+resource "aws_vpc" "vpc_a" {
   cidr_block = "10.100.0.0/16"
+  instance_tenancy = "default"
   tags = {
-    Name = "landing-zone-vpc"
+    Name = "VPC-A"
   }
 }
 
-resource "aws_subnet" "landing_zone_private_subnet" {
-  vpc_id = aws_vpc.landing_zone_vpc.id
+resource "aws_internet_gateway" "vpc_a_igw" {
+  vpc_id = aws_vpc.vpc_a.id
+  tags = {
+    Name = "VPC-A-IGW"
+  }
+}
+
+resource "aws_subnet" "vpc_a_subnet_public" {
+  vpc_id = aws_vpc.vpc_a.id
+  cidr_block = "10.100.0.0/24"
+  tags = {
+    Name = "VPC-A-Subnet-Public"
+  }
+}
+
+resource "aws_route_table" "vpc_a_rt_public" {
+  vpc_id = aws_vpc.vpc_a.id
+  tags = {
+    "Name" = "VPC-A-RT-Public"
+  }
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.vpc_a_igw.id
+  }
+}
+
+resource "aws_route_table_association" "vpc_a_subnet_public_association" {
+  subnet_id      = aws_subnet.vpc_a_subnet_public.id
+  route_table_id = aws_route_table.vpc_a_rt_public.id
+}
+
+resource "aws_subnet" "vpc_a_subnet_private" {
+  vpc_id = aws_vpc.vpc_a.id
   cidr_block = "10.100.1.0/24"
   tags = {
-    Name = "landing-zone-private-subnet"
+    Name = "VPC-A-Subnet-Private"
   }
 }
 
-resource "aws_route_table" "landing_zone_private_subnet" {
-  vpc_id = aws_vpc.landing_zone_vpc.id
+resource "aws_route_table" "vpc_a_rt_private" {
+  vpc_id = aws_vpc.vpc_a.id
+
+  route {
+    cidr_block = aws_vpc.vpc_b.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.vpc_peering.id
+  }
+
   tags = {
-    "Name" = "landing-zone-private-rt"
+    "Name" = "VPC-A-RT-Private"
   }
 }
 
-resource "aws_route_table_association" "landing_zone_private_subnet_association" {
-  subnet_id      = aws_subnet.landing_zone_private_subnet.id
-  route_table_id = aws_route_table.landing_zone_private_subnet.id
+resource "aws_route_table_association" "vpc_a_subnet_private_association" {
+  subnet_id      = aws_subnet.vpc_a_subnet_private.id
+  route_table_id = aws_route_table.vpc_a_rt_private.id
 }
 
-resource "aws_route" "landing_zone_tgw_route" {
-  route_table_id         = aws_route_table.landing_zone_private_subnet.id
-  destination_cidr_block = "10.200.0.0/16"
-  transit_gateway_id     = aws_ec2_transit_gateway.vpc_tgw.id
-  depends_on = [
-    aws_ec2_transit_gateway.vpc_tgw
-  ]
-}
-
-
-resource "aws_vpc" "application_vpc" {
+resource "aws_vpc" "vpc_b" {
   cidr_block = "10.200.0.0/16"
   tags = {
-    Name = "application-vpc"
+    Name = "VPC-B"
   }
 }
 
-resource "aws_subnet" "application_private_subnet" {
-  vpc_id = aws_vpc.application_vpc.id
+resource "aws_subnet" "vpc_b_subnet_private" {
+  vpc_id = aws_vpc.vpc_b.id
   cidr_block = "10.200.1.0/24"
   tags = {
-    Name = "application-private-subnet"
+    Name = "VPC-B-Subnet-Private"
   }
 }
 
-resource "aws_route_table" "application_private_subnet" {
-  vpc_id = aws_vpc.application_vpc.id
+resource "aws_route_table" "vpc_b_rt_private" {
+  vpc_id = aws_vpc.vpc_b.id
+
+  route {
+    cidr_block = aws_vpc.vpc_a.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.vpc_peering.id
+  }
+
   tags = {
-    "Name" = "application-private-rt"
+    "Name" = "VPC-B-RT-Private"
   }
 }
 
-resource "aws_route_table_association" "application_private_subnet_association" {
-  subnet_id      = aws_subnet.application_private_subnet.id
-  route_table_id = aws_route_table.application_private_subnet.id
+resource "aws_route_table_association" "vpc_b_subnet_private_association" {
+  subnet_id      = aws_subnet.vpc_b_subnet_private.id
+  route_table_id = aws_route_table.vpc_b_rt_private.id
 }
 
-resource "aws_route" "application_tgw_route" {
-  route_table_id         = aws_route_table.application_private_subnet.id
-  destination_cidr_block = "10.100.0.0/16"
-  transit_gateway_id     = aws_ec2_transit_gateway.vpc_tgw.id
-  depends_on = [
-    aws_ec2_transit_gateway.vpc_tgw
-  ]
-}
-
-
-# Create tgw in AWS Network Account 
-resource "aws_ec2_transit_gateway" "vpc_tgw" {
-  description                     = "Transit Gateway testing scenario with 2 VPCs, subnets each"
-  default_route_table_association = "enable"
-  default_route_table_propagation = "enable"
-  tags = {
-    Name        = "vpc-tgw"
-  }
-}
-
-# Attachement of Landing Zone VPC from AWS production Account
-resource "aws_ec2_transit_gateway_vpc_attachment" "landing_zone_vpc_attachment" {
-  subnet_ids         = [aws_subnet.landing_zone_private_subnet.id]
-  transit_gateway_id = aws_ec2_transit_gateway.vpc_tgw.id
-  vpc_id             = aws_vpc.landing_zone_vpc.id
-  tags = {
-    "Name" = "landing-zone-transit-gateway-attachment"
-  }
-}
-
-# Attachement of Application VPC from AWS production Account
-resource "aws_ec2_transit_gateway_vpc_attachment" "application_vpc_attachment" {
-  subnet_ids         = [aws_subnet.application_private_subnet.id]
-  transit_gateway_id = aws_ec2_transit_gateway.vpc_tgw.id
-  vpc_id             = aws_vpc.application_vpc.id
-  tags = {
-    "Name" = "application-transit-gateway-attachment"
-  }
-}
-
-
-data "template_file" "startup" {
- template = file("ssm-agent-install.sh")
-}
-
-resource "aws_instance" "landing_zone_instance" {
+resource "aws_instance" "vpc_a_ec2_public" {
   ami = "ami-0b0f138edf421d756"
   instance_type = "t2.micro"
-  subnet_id = aws_subnet.landing_zone_private_subnet.id
-  # key_name = "dicoding-demo"
-  iam_instance_profile = aws_iam_instance_profile.dev_resources_iam_profile.name
-  user_data = data.template_file.startup.rendered
-
+  subnet_id = aws_subnet.vpc_a_subnet_public.id
+  key_name = "demo-app"
+  associate_public_ip_address = true
+  vpc_security_group_ids = [aws_security_group.vpc_a_sg_public.id]
   tags = {
-    Name = "landing-zone-instance"
+    Name = "VPC-A-EC2-Public"
   }
 }
 
-resource "aws_instance" "application_instance" {
-  ami           = "ami-0b0f138edf421d756"
+resource "aws_security_group" "vpc_a_sg_public" {
+  name = "VPC-A-SG-Public"
+  description = "VPC-A-SG-Public"
+  vpc_id = aws_vpc.vpc_a.id
+
+	egress {
+		from_port = 0
+		to_port = 0
+		protocol = -1
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+
+	ingress {
+		from_port = 22
+		to_port = 22
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+
+	# ingress {
+	# 	from_port = 80
+	# 	to_port = 80
+	# 	protocol = "tcp"
+	# 	cidr_blocks = ["0.0.0.0/0"]
+	# }
+
+  tags = {
+    Name = "VPC-A-SG-Public"
+  }
+}
+
+resource "aws_instance" "vpc_a_ec2_private" {
+  ami = "ami-0b0f138edf421d756"
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.application_private_subnet.id
-  # key_name = "dicoding-demo"
-
+  subnet_id = aws_subnet.vpc_a_subnet_private.id
+  key_name = "demo-app"
+  associate_public_ip_address = false
+  vpc_security_group_ids = [aws_security_group.vpc_a_sg_private.id]
   tags = {
-    Name = "application-instance"
+    Name = "VPC-A-EC2-Private"
   }
 }
 
-resource "aws_iam_instance_profile" "dev_resources_iam_profile" {
-  name = "ec2_profile"
-  role = aws_iam_role.dev_resources_iam_role.name
-}
+resource "aws_security_group" "vpc_a_sg_private" {
+  name = "VPC-A-SG-Private"
+  description = "VPC-A-SG-Private"
+  vpc_id = aws_vpc.vpc_a.id
 
-resource "aws_iam_role" "dev_resources_iam_role" {
-  name        = "dev-ssm-role"
-  description = "The role for the developer resources EC2"
-  assume_role_policy = <<EOF
-{
-"Version": "2012-10-17",
-"Statement": {
-"Effect": "Allow",
-"Principal": {"Service": "ec2.amazonaws.com"},
-"Action": "sts:AssumeRole"
-}
-}
-EOF
+  ingress {
+    description = "Allow inbound traffic from Public Subnet"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["10.100.0.0/24"]
+  }
+
   tags = {
-    stack = "test"
+    Name = "VPC-A-SG-Private"
   }
 }
 
-resource "aws_iam_role_policy_attachment" "dev-resources-ssm-policy" {
-  role       = aws_iam_role.dev_resources_iam_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+resource "aws_instance" "vpc_b_ec2_private" {
+  ami = "ami-0b0f138edf421d756"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.vpc_b_subnet_private.id
+  key_name = "demo-app"
+  associate_public_ip_address = false
+  vpc_security_group_ids = [aws_security_group.vpc_b_sg_private.id]
+  tags = {
+    Name = "VPC-B-EC2-Private"
+  }
+}
+
+resource "aws_security_group" "vpc_b_sg_private" {
+  name = "VPC-B-SG-Private"
+  description = "VPC-B-SG-Private"
+  vpc_id = aws_vpc.vpc_b.id
+
+  ingress {
+    description = "Allow inbound traffic from Private Subnet"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["10.100.1.0/24"]
+  }
+
+  tags = {
+    Name = "VPC-B-SG-Private"
+  }
+}
+
+resource "aws_vpc_peering_connection" "vpc_peering" {
+  vpc_id = aws_vpc.vpc_a.id
+  peer_vpc_id = aws_vpc.vpc_b.id
+  auto_accept = true
+}
+
+# resource "aws_key_pair" "demo_app_key_pair" {
+# 	key_name = "demo-app"
+# 	public_key = file("demo-app.pub")
+# }
+
+output "instance_a_public_ip_addr" {
+  value = aws_instance.vpc_a_ec2_public.public_ip
+}
+
+output "instance_a_private_ip_addr" {
+  value = aws_instance.vpc_a_ec2_private.private_ip
+}
+
+output "instance_b_private_ip_addr" {
+  value = aws_instance.vpc_b_ec2_private.private_ip
 }
